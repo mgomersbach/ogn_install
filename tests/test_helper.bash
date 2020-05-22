ecd840e987438fdf46a704770c4e898df6912c29
Mark Gomersbach <markgomersbach@gmail.com>
2020-05-22 14:16:48 +0200

    fix(ci): no apt update for test
    Signed-off-by: Mark Gomersbach <markgomersbach@gmail.com>

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -3,9 +3,6 @@
 test_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH="$test_directory/..:$PATH"
 
-# Add apt sources
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-
 d() {
   mkdir -p "${1}"
 }
