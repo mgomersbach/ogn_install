0676eef476c0f02d1f941ecfff39ff93d0e236ca
Mark Gomersbach <markgomersbach@gmail.com>
2020-05-23 15:01:06 +0200

    fix(ci): just try lighttpd in helper
    Signed-off-by: Mark Gomersbach <markgomersbach@gmail.com>

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -4,7 +4,7 @@ test_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH="$test_directory/..:$PATH"
 
 # Workaround for Github Ubuntu test env
-apt install libssl1.0-dev
+apt install lighttpd
 
 d() {
   mkdir -p "${1}"
